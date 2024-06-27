4610325a681acf5f3d66e1a50258c1d4b6d77889
gmnate6 <gmnate6@gmail.com>
2024-06-27 00:10:18 -0400

    Update Tests

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,10 @@
-from CustomMenuAPI import CustomMenu
+from ..CustomMenuAPI import CustomMenu
 
-# Main Code
-if __name__ == "__main__":
+def test():
     custom_menu = CustomMenu(name="You ____ Custom Menu!", choices=["Love", "Like"])
     response = custom_menu.ask()
     print(f"You {response} It!")
+
+# Main Code
+if __name__ == "__main__":
+    test()
